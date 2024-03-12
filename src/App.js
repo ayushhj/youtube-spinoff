5f37577f6e3da89ec0c3a8f7578690650f840178
ayushhj <ayushjoshi494@gmail.com>
2024-03-13 01:36:48 +0530

    added redux store and added sidebar toggle feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 function App() {
   return (
+
+    <Provider store={store}  >
+
+    
     <div className=""  >
       <Header/>
       <Body/>
 
    
     </div>
+    </Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleMenu } from '../utils/appSlice'
 
 const Header = () => {
+
+  const dispatch = useDispatch()
+
+const toggleMenuHandler = ()=>{
+  dispatch(toggleMenu());
+
+};
+
+
   return (
     <div className='grid grid-flow-col shadow-lg '  >
 
 <div className='flex  col-span-1 m-2' >
-<img className='h-6 m-3 '  alt='menu' src='https://imgs.search.brave.com/VDkctdkNatiSmpjLTAb2HrWzP0OpePN5IlxNVLtk7Nw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzA5LzQ1Lzgw/LzM2MF9GXzEwOTQ1/ODAxNV9Rc1dtY2hs/enV3Q1pQcUlVV1I3/SGNURHNiYnB0ZWpS/di5qcGc' />
-<img className='h-12' alt='youtubeicon' src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg" />
+<img className='h-6 m-3  cursor-pointer'  
+onClick={toggleMenuHandler}
+alt='menu' 
+src='https://imgs.search.brave.com/VDkctdkNatiSmpjLTAb2HrWzP0OpePN5IlxNVLtk7Nw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzA5LzQ1Lzgw/LzM2MF9GXzEwOTQ1/ODAxNV9Rc1dtY2hs/enV3Q1pQcUlVV1I3/SGNURHNiYnB0ZWpS/di5qcGc' />
+<img className='h-12 cursor-pointer' alt='youtubeicon' src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg" />
 </div>
 
 <div className='col-span-10 px-10 ' >
