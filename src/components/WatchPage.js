a0d8ab20fe1ee5b4baf979f2eee1df1ea39962f8
ayushhj <ayushjoshi494@gmail.com>
2024-03-19 12:15:13 +0530

    implemented comments with n-level nesting

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice'
 import { useSearchParams } from 'react-router-dom'
+import CommentContainer from './CommentContainer'
 
 const WatchPage = () => {
 
@@ -17,7 +18,9 @@ dispatch(closeMenu())
   return (
     <div>
       <iframe width="1000" height="600" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-
+<div>
+  <CommentContainer/>
+</div>
     </div>
   )
 }
