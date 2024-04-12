aaeeacc62f3a5c966bd4068963545c1410539dee
ayushhj <ayushjoshi494@gmail.com>
2024-04-12 12:54:12 +0530

    added live chat feature using API polling

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,13 +1,37 @@
-import ChatMessage from "./ChatMessage"
+import { useEffect } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
 
-const LiveChat = ()=>{
-    return(
-        <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg "   >
-            <ChatMessage  
-            name="Ayush Joshi"
-            message="Learning React"  />
-        </div>
-    )
-}
+const LiveChat = () => {
 
-export default LiveChat
+    const dispatch = useDispatch();
+    const ChatMessages = useSelector((store)=>store.chat.messages)
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      //API Polling
+
+        dispatch(
+            addMessage({
+                name: "Ayush",
+                message : "Hello checking live chat"
+            })
+        )
+
+    }, 2000);
+    return () => clearInterval(i);
+  }, []);
+
+  return (
+    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll ">
+      {
+        ChatMessages.map((c,i)=>(
+            <ChatMessage key={i} name={c.name} message={c.message} />
+        ))
+      }
+    </div>
+  );
+};
+
+export default LiveChat;
